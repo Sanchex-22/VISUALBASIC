a0b795fa38fa7add891b13d7a0189970b2425d53
Sanchex-22 <David21.cs70@gmail.com>
2023-10-08 17:15:52 -0500

    creando repo

---
+++ JuegoMatematico/Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        btn_Generar = New Button()
+        txtResultado = New TextBox()
+        txtElementos = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Timer1 = New Timer(components)
+        TextBox1 = New TextBox()
+        btnBorrar = New Button()
+        txtInput = New TextBox()
+        btnSiguiente = New Button()
+        Label1 = New Label()
+        res1 = New TextBox()
+        res2 = New TextBox()
+        btnEnviar = New Button()
+        Label2 = New Label()
+        btnBorrarTodo = New Button()
+        txtUserResult = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btn_Generar
+        ' 
+        btn_Generar.Location = New Point(466, 54)
+        btn_Generar.Name = "btn_Generar"
+        btn_Generar.Size = New Size(75, 23)
+        btn_Generar.TabIndex = 0
+        btn_Generar.Text = "Empezar"
+        btn_Generar.UseVisualStyleBackColor = True
+        ' 
+        ' txtResultado
+        ' 
+        txtResultado.Location = New Point(385, 54)
+        txtResultado.Name = "txtResultado"
+        txtResultado.Size = New Size(75, 23)
+        txtResultado.TabIndex = 1
+        ' 
+        ' txtElementos
+        ' 
+        txtElementos.Location = New Point(223, 54)
+        txtElementos.Name = "txtElementos"
+        txtElementos.Size = New Size(156, 23)
+        txtElementos.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(223, 165)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 3
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(223, 194)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 4
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(223, 223)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 5
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(223, 252)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 6
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(304, 165)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 23)
+        Button5.TabIndex = 7
+        Button5.Text = "Button5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(304, 194)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(75, 23)
+        Button6.TabIndex = 8
+        Button6.Text = "Button6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(304, 223)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(75, 23)
+        Button7.TabIndex = 9
+        Button7.Text = "Button7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(304, 252)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(75, 23)
+        Button8.TabIndex = 10
+        Button8.Text = "Button8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(115, 6)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(37, 23)
+        TextBox1.TabIndex = 11
+        ' 
+        ' btnBorrar
+        ' 
+        btnBorrar.Location = New Point(466, 135)
+        btnBorrar.Name = "btnBorrar"
+        btnBorrar.Size = New Size(75, 23)
+        btnBorrar.TabIndex = 12
+        btnBorrar.Text = "Borrar"
+        btnBorrar.UseVisualStyleBackColor = True
+        ' 
+        ' txtInput
+        ' 
+        txtInput.Location = New Point(223, 136)
+        txtInput.Name = "txtInput"
+        txtInput.Size = New Size(156, 23)
+        txtInput.TabIndex = 13
+        ' 
+        ' btnSiguiente
+        ' 
+        btnSiguiente.Location = New Point(713, 415)
+        btnSiguiente.Name = "btnSiguiente"
+        btnSiguiente.Size = New Size(75, 23)
+        btnSiguiente.TabIndex = 14
+        btnSiguiente.Text = "Siguiente"
+        btnSiguiente.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(11, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(98, 15)
+        Label1.TabIndex = 15
+        Label1.Text = "Tiempo Restante:"
+        ' 
+        ' res1
+        ' 
+        res1.Location = New Point(547, 55)
+        res1.Name = "res1"
+        res1.Size = New Size(61, 23)
+        res1.TabIndex = 16
+        ' 
+        ' res2
+        ' 
+        res2.Location = New Point(547, 84)
+        res2.Name = "res2"
+        res2.Size = New Size(61, 23)
+        res2.TabIndex = 17
+        ' 
+        ' btnEnviar
+        ' 
+        btnEnviar.Location = New Point(223, 281)
+        btnEnviar.Name = "btnEnviar"
+        btnEnviar.Size = New Size(156, 23)
+        btnEnviar.TabIndex = 18
+        btnEnviar.Text = "Enviar"
+        btnEnviar.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(229, 5)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(197, 15)
+        Label2.TabIndex = 19
+        Label2.Text = "Adivina el resultado de la Operacion"
+        ' 
+        ' btnBorrarTodo
+        ' 
+        btnBorrarTodo.Location = New Point(385, 165)
+        btnBorrarTodo.Name = "btnBorrarTodo"
+        btnBorrarTodo.Size = New Size(156, 23)
+        btnBorrarTodo.TabIndex = 20
+        btnBorrarTodo.Text = "Borrar Todo"
+        btnBorrarTodo.UseVisualStyleBackColor = True
+        ' 
+        ' txtUserResult
+        ' 
+        txtUserResult.Location = New Point(385, 136)
+        txtUserResult.Name = "txtUserResult"
+        txtUserResult.Size = New Size(75, 23)
+        txtUserResult.TabIndex = 21
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtUserResult)
+        Controls.Add(btnBorrarTodo)
+        Controls.Add(Label2)
+        Controls.Add(btnEnviar)
+        Controls.Add(res2)
+        Controls.Add(res1)
+        Controls.Add(Label1)
+        Controls.Add(btnSiguiente)
+        Controls.Add(txtInput)
+        Controls.Add(btnBorrar)
+        Controls.Add(TextBox1)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(txtElementos)
+        Controls.Add(txtResultado)
+        Controls.Add(btn_Generar)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btn_Generar As Button
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents txtElementos As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnSiguiente As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents res1 As TextBox
+    Friend WithEvents res2 As TextBox
+    Friend WithEvents btnEnviar As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnBorrarTodo As Button
+    Friend WithEvents txtUserResult As TextBox
+End Class
---
+++ JuegoMatematico/Form1.vb
@@ -0,0 +1,354 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar
+
+Public Class Form1
+    Dim segundosRestantes As Integer = 60 ' Inicializar con 60 segundos
+    Dim resultado As Double
+    'Dim resultado_user As Double
+    Dim elementosGenerados As String
+    ' Dim botones() As Button = {Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8}
+    Dim operadores() As String = {"+", "-", "*", "/"}
+    'Dim datos() As String
+    Dim valores As New List(Of String)
+    'Dim datos As New List(Of String)()
+
+    Public Sub New()
+        InitializeComponent()
+        btnSiguiente.Enabled = False
+        btnOffNum()
+        btnOffOp()
+        btnEnviar.Enabled = False
+        txtInput.ReadOnly = True
+        TextBox1.ReadOnly = True
+    End Sub
+
+    Public Sub btnOnNum()
+        Button1.Enabled = True
+        Button3.Enabled = True
+        Button5.Enabled = True
+        Button7.Enabled = True
+    End Sub
+
+    Public Sub btnOffNum()
+        Button1.Enabled = False
+        Button3.Enabled = False
+        Button5.Enabled = False
+        Button7.Enabled = False
+    End Sub
+
+    Public Sub btnOnOp()
+        Button2.Enabled = True
+        Button4.Enabled = True
+        Button6.Enabled = True
+        Button8.Enabled = True
+    End Sub
+
+    Public Sub btnOffOp()
+        Button2.Enabled = False
+        Button4.Enabled = False
+        Button6.Enabled = False
+        Button8.Enabled = False
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Configurar el intervalo del temporizador (1000 ms = 1 segundo)
+        Timer1.Interval = 1000
+    End Sub
+
+    Private Sub btnGenerar_Click(sender As Object, e As EventArgs) Handles btn_Generar.Click
+        Timer1.Start()
+        Comenzar()
+        btn_Generar.Enabled = False
+    End Sub
+
+    Public Sub ActiveDisablebtn(btns As String)
+        Dim btn As String = btns
+        If btn = "0" Or btn = "1" Or btn = "2" Or btn = "3" Or btn = "4" Or btn = "5" Or btn = "6" Or btn = "7" Or btn = "8" Or btn = "9" Then
+            btnOffNum()
+            btnOnOp()
+        ElseIf btn = "+" Or btn = "*" Or btn = "-" Or btn = "/" Then
+            btnOffOp()
+            btnOnNum()
+        End If
+    End Sub
+
+    'funcion para que empieze el juego
+    Public Sub Comenzar()
+        ' Crear un arreglo para almacenar números y operadores intercalados
+        Dim elementos(7) As Object
+        Dim generadorAleatorio As New Random()
+
+        ' Generar 4 números aleatorios y 3 operadores aleatorios
+        For i As Integer = 0 To 3
+            elementos(i * 2) = generadorAleatorio.Next(1, 9) ' Números aleatorios entre 1 y 100
+            ' Generar operadores aleatorios
+            elementos(i * 2 + 1) = operadores(generadorAleatorio.Next(0, operadores.Length))
+        Next
+
+        ' Mostrar los números y operadores en un cuadro de texto o en la consola
+        elementosGenerados = String.Join(" ", elementos.Take(7))
+        txtElementos.Text = elementosGenerados
+
+        ' Llenar los botones con datos generados
+        Button1.Text = elementos(0)
+        Button3.Text = elementos(2)
+        Button5.Text = elementos(4)
+        Button7.Text = elementos(6)
+        Button2.Text = operadores(0)
+        Button4.Text = operadores(1)
+        Button6.Text = operadores(2)
+        Button8.Text = operadores(3)
+        btnOnNum()
+        ' btnOnOp()
+        btnEnviar.Enabled = True
+        ' Realizar la operación matemática y guardar el resultado
+        resultado = CalcularResultado(elementos)
+        txtResultado.Text = resultado.ToString()
+    End Sub
+
+    ' Función para calcular el resultado de la operación matemática
+    Private Function CalcularResultado(elementos As Object()) As Double
+        ' Guardar los elementos en variables individuales
+        Dim num1 As Double = Convert.ToDouble(elementos(0))
+        Dim operador1 As String = Convert.ToString(elementos(1))
+        Dim num2 As Double = Convert.ToDouble(elementos(2))
+        Dim operador2 As String = Convert.ToString(elementos(3))
+        Dim num3 As Double = Convert.ToDouble(elementos(4))
+        Dim operador3 As String = Convert.ToString(elementos(5))
+        Dim num4 As Double = Convert.ToDouble(elementos(6))
+
+
+        ' Realizar la operación en el orden especificado
+        Dim resultado1 As Double
+        Dim resultado2 As Double
+        Dim resultadoma As Double
+
+        resultado1 = Calculo(num1, num2, operador1)
+        res1.Text = resultado1.ToString()
+        resultado2 = Calculo(num3, num4, operador3)
+        res2.Text = resultado2.ToString()
+        resultadoma = Calculo(resultado1, resultado2, operador2)
+
+        Return resultadoma
+    End Function
+
+    Public Function Calculo(num1 As Double, num2 As Double, operador As String)
+        Dim resultado As Double
+        If operador = "/" Then
+            resultado = num1 / num2
+            Return resultado
+        ElseIf operador = "*" Then
+            resultado = num1 * num2
+            Return resultado
+        ElseIf operador = "-" Then
+            resultado = num1 - num2
+            Return resultado
+        ElseIf operador = "+" Then
+            resultado = num1 + num2
+            Return resultado
+        Else
+            Console.WriteLine("El operador no es válido.")
+        End If
+        Return resultado
+    End Function
+
+    Private Sub Timer1_Tick1(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' Actualizar el contador de segundos restantes
+        segundosRestantes -= 1
+
+        ' Mostrar el tiempo restante en un control TextBox (o donde desees)
+        TextBox1.Text = segundosRestantes.ToString() & " segundos"
+
+        ' Comprobar si el tiempo ha llegado a cero
+        If segundosRestantes = 0 Then
+            ' El tiempo ha terminado, puedes tomar acciones aquí
+            Timer1.Stop() ' Detener el temporizador si es necesario
+            MessageBox.Show("¡Tiempo agotado!")
+        End If
+    End Sub
+
+    Private Sub Actualizar()
+        txtInput.Text = String.Join(", ", valores)
+    End Sub
+
+    Private Sub btnSiguiente_Click(sender As Object, e As EventArgs) Handles btnSiguiente.Click
+        segundosRestantes = segundosRestantes + 60
+        Comenzar()
+        btnSiguiente.Enabled = True
+    End Sub
+
+
+    '--------------------BOTONES--------------------
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim valorDelBoton As String = DirectCast(sender, Windows.Forms.Button).Text
+        If valores.Count < 7 Then
+            'MessageBox.Show(valores.Count)
+            valores.Add(valorDelBoton)
+            ' Puedes realizar acciones adicionales si lo deseas, como mostrar los valores en un TextBox
+            txtInput.Text = String.Join(", ", valores)
+            Actualizar()
+            ActiveDisablebtn(valorDelBoton)
+        Else
+            MessageBox.Show("La lista ya contiene 8 elementos. No se pueden agregar más.")
+            btnOffOp()
+            btnOffNum()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim valorDelBoton As String = DirectCast(sender, Windows.Forms.Button).Text
+        If valores.Count < 7 Then
+            'MessageBox.Show(valores.Count)
+            valores.Add(valorDelBoton)
+            ' Puedes realizar acciones adicionales si lo deseas, como mostrar los valores en un TextBox
+            txtInput.Text = String.Join(", ", valores)
+            Actualizar()
+            ActiveDisablebtn(valorDelBoton)
+        Else
+            MessageBox.Show("La lista ya contiene 8 elementos. No se pueden agregar más.")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim valorDelBoton As String = DirectCast(sender, Windows.Forms.Button).Text
+        If valores.Count < 7 Then
+            valores.Add(valorDelBoton)
+            ' Puedes realizar acciones adicionales si lo deseas, como mostrar los valores en un TextBox
+            txtInput.Text = String.Join(", ", valores)
+            Actualizar()
+            ActiveDisablebtn(valorDelBoton)
+        Else
+            MessageBox.Show("La lista ya contiene 8 elementos. No se pueden agregar más.")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim valorDelBoton As String = DirectCast(sender, Windows.Forms.Button).Text
+        If valores.Count < 7 Then
+            valores.Add(valorDelBoton)
+            ' Puedes realizar acciones adicionales si lo deseas, como mostrar los valores en un TextBox
+            txtInput.Text = String.Join(", ", valores)
+            Actualizar()
+            ActiveDisablebtn(valorDelBoton)
+        Else
+            MessageBox.Show("La lista ya contiene 8 elementos. No se pueden agregar más.")
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim valorDelBoton As String = DirectCast(sender, Windows.Forms.Button).Text
+        If valores.Count < 7 Then
+            valores.Add(valorDelBoton)
+            ' Puedes realizar acciones adicionales si lo deseas, como mostrar los valores en un TextBox
+            txtInput.Text = String.Join(", ", valores)
+            Actualizar()
+            ActiveDisablebtn(valorDelBoton)
+        Else
+            MessageBox.Show("La lista ya contiene 8 elementos. No se pueden agregar más.")
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim valorDelBoton As String = DirectCast(sender, Windows.Forms.Button).Text
+        If valores.Count < 7 Then
+            valores.Add(valorDelBoton)
+            ' Puedes realizar acciones adicionales si lo deseas, como mostrar los valores en un TextBox
+            txtInput.Text = String.Join(", ", valores)
+            Actualizar()
+            ActiveDisablebtn(valorDelBoton)
+        Else
+            MessageBox.Show("La lista ya contiene 8 elementos. No se pueden agregar más.")
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim valorDelBoton As String = DirectCast(sender, Windows.Forms.Button).Text
+        If valores.Count < 7 Then
+            valores.Add(valorDelBoton)
+            ' Puedes realizar acciones adicionales si lo deseas, como mostrar los valores en un TextBox
+            txtInput.Text = String.Join(", ", valores)
+            Actualizar()
+            ActiveDisablebtn(valorDelBoton)
+        Else
+            MessageBox.Show("La lista ya contiene 8 elementos. No se pueden agregar más.")
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim valorDelBoton As String = DirectCast(sender, Windows.Forms.Button).Text
+        If valores.Count < 7 Then
+            valores.Add(valorDelBoton)
+            ' Puedes realizar acciones adicionales si lo deseas, como mostrar los valores en un TextBox
+            txtInput.Text = String.Join(", ", valores)
+            Actualizar()
+            ActiveDisablebtn(valorDelBoton)
+        Else
+            MessageBox.Show("La lista ya contiene 8 elementos. No se pueden agregar más.")
+        End If
+    End Sub
+
+    Private Sub btnBorrarTodo_Click(sender As Object, e As EventArgs) Handles btnBorrarTodo.Click
+        If valores.Count > 0 Then
+            ' Eliminar todos los elementos de la lista
+            valores.Clear()
+
+            ' Actualizar el TextBox para mostrar la lista vacía
+            btnOffOp()
+            btnOnNum()
+            Actualizar()
+        Else
+            MessageBox.Show("No hay valores para eliminar.")
+        End If
+    End Sub
+
+    'Boton que borra el ultimo elemento del array
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        If valores.Count > 0 Then
+            ' Eliminar el último valor agregado
+            ActiveDisablebtnDelete(valores.Last())
+            valores.RemoveAt(valores.Count - 1)
+            ' Actualizar el TextBox para mostrar los valores actualizados
+            Actualizar()
+        Else
+            MessageBox.Show("No hay valores para eliminar.")
+        End If
+    End Sub
+
+    Public Sub ActiveDisablebtnDelete(btns As String)
+        Dim btn As String = btns
+        If btn = "0" Or btn = "1" Or btn = "2" Or btn = "3" Or btn = "4" Or btn = "5" Or btn = "6" Or btn = "7" Or btn = "8" Or btn = "9" Then
+            btnOffOp()
+            btnOnNum()
+        ElseIf btn = "+" Or btn = "*" Or btn = "-" Or btn = "/" Then
+            btnOnOp()
+            btnOffNum()
+        End If
+    End Sub
+
+    Private Sub btnEnviar_Click(sender As Object, e As EventArgs) Handles btnEnviar.Click
+        If valores.Count > 6 Then
+            Comparar()
+        Else
+            MessageBox.Show(valores.Count)
+            MessageBox.Show("Ingrese todos los valores")
+        End If
+    End Sub
+
+    Public Function Comparar()
+        MessageBox.Show("Calculando...")
+        Dim array() As String = valores.ToArray()
+        Dim resultado_user = CalcularResultado(array)
+        txtUserResult.Text = resultado_user
+        If resultado = resultado_user Then
+            btnSiguiente.Enabled = True
+            MessageBox.Show("Resultado Correcto (Presione Siguiente)")
+        Else
+            MessageBox.Show("Resultado erroneo Intente Denuevo")
+        End If
+    End Function
+
+
+    Public Function Limpiar()
+
+    End Function
+End Class
